cbf1904738d7455f848dae671b8030470ed3c910
xapis-21 <78486475+xapis-21@users.noreply.github.com>
2022-09-24 17:26:54 +0300

    added usecallback

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useRouter } from "next/router";
 import { BsTelephoneFill, BsFillEnvelopeFill } from "react-icons/bs";
 import { HiOutlineMenu} from "react-icons/hi";
