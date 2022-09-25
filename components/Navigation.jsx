fab207f916201bab18aed380a25dab4c59c300e4
xapis-21 <78486475+xapis-21@users.noreply.github.com>
2022-09-25 12:13:31 +0300

    fixing nav 2

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -94,7 +94,7 @@ const Navigation = () => {
         </div>
       </nav>
       <nav className="bg-green-dark/80 backdrop-blur-lg px-2 sm:px-4 py-2 max-w-[1720px] w-full rounded-b-lg border-t-2 border-white/80">
-        <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
+        <ul className="flex flex-col bg-gray-50 md:hidden">
           {navlinks.map(([path, pathlink]) => (
             <Link href={pathlink} key={pathlink}>
               <a
